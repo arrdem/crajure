f4feb5706abc7783686302b122e424d5a495b234
Reid 'arrdem' McKenzie <me@arrdem.com>
2016-05-23 21:17:27 -0500

    Going my own way

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject crajure "0.1.1"
+(defproject me.arrdem/crajure "0.1.1"
   :description "An extremely simple interface to scrape craigslist. How you use it may be against their T.O.S."
   :url "http://www.github.com/escherize/crajure"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [enlive              "1.1.5"]]
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [enlive              "1.1.6"]]
   :deploy-repositories [["releases" :clojars]])
