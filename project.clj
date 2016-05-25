5caaf3e3ce23e6ddc061895b4fca1b88a12b4e5e
Reid 'arrdem' McKenzie <me@arrdem.com>
2016-05-25 17:49:54 -0500

    Add & use a request cache for listing pgs

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [enlive              "1.1.6"]
-                 [me.arrdem/rate-gate "1.3.1"]]
+                 [me.arrdem/rate-gate "1.3.1"]
+                 [pandect "0.6.0"]]
   :deploy-repositories [["releases" :clojars]])
--- src/crajure/util.clj
+++ src/crajure/util.clj
@@ -1,19 +1,42 @@
 (ns crajure.util
   (:require [net.cgrand.enlive-html :as html]
-            [rate-gate.core :refer [rate-limit]]))
+            [rate-gate.core :refer [rate-limit]]
+            [pandect.algo.sha256 :as sha]
+            [clojure.java.io :as io]))
 
 (defonce
   ^{:arglists '([url & properties])}
-  fetch-url
+  fetch-url*
   (rate-limit
    (fn [url & properties]
-     (with-open [inputstream (-> (java.net.URL. url)
-                                 .openConnection
-                                 (doto (.setRequestProperty
-                                        "User-Agent" "Mozilla/5.0"))
-                                 .getContent)]
-       (html/html-resource inputstream)))
+     (-> (java.net.URL. url)
+         .openConnection
+         (doto (.setRequestProperty "User-Agent" "Mozilla/5.0"))
+         .getContent))
    2 1000))
+
+(defn url->f [url]
+  (.mkdirs (io/file "cache"))
+  (io/file (str "cache/" (sha/sha256 url) ".html")))
+
+(defn cache-fetch-url [url]
+  (let [f (url->f url)]
+    (when (.exists f)
+      (io/input-stream f))))
+
+(defn cache-put-url [url data]
+  (spit (url->f url) data))
+
+(defn fetch-url [url & properties]
+  (or (when (.endsWith url ".html")
+        (some-> (cache-fetch-url url)
+                (html/html-resource)))
+      (let [res (apply fetch-url* url properties)]
+        (when (.endsWith url ".html")
+          (let [sw (java.io.StringWriter.)]
+            (io/copy res sw)
+            (cache-put-url url (.toString sw))))
+        (fetch-url url))))
 
 (defn has-page? [url]
   (boolean
