79d32de6bd9721f91933b3d68b57ab87403240d1
Reid 'arrdem' McKenzie <me@arrdem.com>
2016-05-24 21:38:52 -0500

    Tweak tests some

--- test/crajure/core_test.clj
+++ test/crajure/core_test.clj
@@ -2,21 +2,16 @@
   (:require [clojure.test :refer :all]
             [crajure.core :refer :all]))
 
-(def get-pages
-  (fn []
-    (query-cl {:query "fixie bike"
-               :area "sfbay"
-               :section :for-sale})))
-
 (defn unsearchable-string []
   (str (repeat 10 (java.util.UUID/randomUUID))))
 
 (deftest cl-query-works
   (testing "all maps recieved from query-result are identical and listed."
-    (let [query-result   (get-pages)
-          keys-in-result (->> query-result (map keys) set)]
-      (is (= #{[:price :title :date :region :item-url]}
-             keys-in-result))))
+    (let [query-result   (query-cl {:query   "fixie bike"
+                                    :area    "sfbay"
+                                    :section :for-sale/all})
+          keys-in-result (->> query-result (mapcat keys) set)]
+      (is (= #{:price :title :date :region :url :preview} keys-in-result))))
   (testing "no results returns empty seq"
-    (let [empty-results (query-cl (unsearchable-string) "sfbay" :for-sale)]
+    (let [empty-results (query-cl (unsearchable-string) "sfbay" :for-sale/all)]
       (is (empty? empty-results)))))
