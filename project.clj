fa972b5f32ff01d7a36dffd927ca6f40972fc240
Reid 'arrdem' McKenzie <me@arrdem.com>
2016-05-30 19:10:26 -0700

    Bump

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject me.arrdem/crajure "0.3.2"
+(defproject me.arrdem/crajure "0.3.3"
   :description "An extremely simple interface to scrape craigslist. How you use it may be against their T.O.S."
   :url "http://www.github.com/arrdem/crajure"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [enlive              "1.1.6"]
+                 [enlive "1.1.6"]
                  [me.arrdem/rate-gate "1.3.1"]
                  [pandect "0.6.0"]]
   :deploy-repositories [["releases" :clojars]])
